140737c2f2d74eb97d333f8fcf797a199247642e
Dion Solang <gumball3point@gmail.com>
2024-02-20 08:59:51 +0700

    contructor overloading

--- Constructor/Program.cs
+++ Constructor/Program.cs
@@ -4,13 +4,20 @@ class A{
 
     public string foo;
     public int bar;
+
+    public int dar;
+
     public A(string foo, int bar){
-        Console.WriteLine("an instance of "+ this + " is built");
+        Console.WriteLine("an instance of "+ this + " is built with foobar");
         this.foo = foo;
         this.bar = bar;
     }
-    public void tobeoverrided(){
-        Console.WriteLine("hello");
+
+    public A(string foo, int bar, int dar){
+        Console.WriteLine("an instance of "+ this + " is built with foobardar");
+        this.foo = foo;
+        this.bar = bar;
+        this.dar = dar;
     }
 }
 
@@ -21,8 +28,13 @@ class Program{
 
         Console.WriteLine(obj.foo);
         Console.WriteLine(obj.bar);
+        Console.WriteLine(obj.dar);
 
-        obj.tobeoverrided();
+
+        A obj2 = new A("ohla",12,13) ;
+        Console.WriteLine(obj2.foo);
+        Console.WriteLine(obj2.bar);
+        Console.WriteLine(obj2.dar);
 
         return -1;
     }
