62ee94ca72abcd98cce3af806b8359a7b5998e7e
Dion Solang <gumball3point@gmail.com>
2024-02-20 08:21:24 +0700

    first commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+class Calculator {
+
+    public int offsets = 0;
+    public int s = 0;
+    private int testPrivate = 0;
+
+    public int Add(int a, int b){
+        return a+b + this.s; 
+    }
+    public int Mul(int a, int b){
+        return a*b*this.s;
+    }
+
+}
+
+
+class Program{
+
+    static void Main(){
+        Calculator calc = new Calculator() {
+            offsets = 1
+        };
+        string s = "test " + (new Calculator()) + (new Calculator()) ;
+        int result = calc.Add(1,1);
+        Console.WriteLine(s);
+        Console.WriteLine(result);
+    }
+}
---
+++ first-project/Program.cs
@@ -0,0 +1,38 @@
+﻿class Car {
+    public string name;
+    public float speed;
+    public float height;
+
+    public void PrintName(){
+        Console.WriteLine(name);
+    }
+
+    public void Start(){
+        Console.WriteLine("start");
+    }
+
+    public void Gas(){
+        Console.WriteLine("Gass");
+    }
+
+    public void Stop(){
+        Console.WriteLine("stop");
+    }
+    
+}
+
+
+
+
+class Program{
+    static void Main(){
+        Car gato = new Car();
+        gato.name = "testname";
+        gato.name += "hahaha";
+        gato.name = "hahaha"+gato.name;
+        gato.PrintName();
+        gato.Start();
+        gato.Gas();
+        gato.Stop();
+    }
+}
