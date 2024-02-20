b7ed5ccbedd22697189d095910e8392b9152bb2c
Dion Solang <gumball3point@gmail.com>
2024-02-20 08:41:25 +0700

    Exploring Constructor 1

---
+++ Constructor/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class A{
+
+    public string foo;
+    public int bar;
+    public A(string foo, int bar){
+        Console.WriteLine("an instance of "+ this + " is built");
+        this.foo = foo;
+        this.bar = bar;
+    }
+    public void tobeoverrided(){
+        Console.WriteLine("hello");
+    }
+}
+
+class Program{
+
+    static void Main(){
+        A obj = new A("ohla",12);
+
+        Console.WriteLine(obj.foo);
+        Console.WriteLine(obj.bar);
+
+        obj.tobeoverrided();
+
+        return;
+    }
+
+
+}
