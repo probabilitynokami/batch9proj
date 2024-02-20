c94219ea5ef19c615a4485f436ab0cf0cd3af3c5
Dion Solang <gumball3point@gmail.com>
2024-02-20 14:22:54 +0700

    class library demo

---
+++ CalculatorLib/Calculator.cs
@@ -0,0 +1,24 @@
+﻿namespace CalculatorLib;
+
+public class Calculator
+{
+    public int Calculate(int a, int b, IOperation op){
+        return op.Operate(a,b);
+    }
+}
+
+public abstract class IOperation{
+    public abstract int Operate(int a, int b);
+}
+
+public class Addition : IOperation{
+    public override int Operate(int a, int b){
+        return a+b;
+    }
+}
+public class Multiplication : IOperation{
+    public override int Operate(int a, int b){
+        return a*b;
+    }
+}
+
---
+++ CalculatorProgram/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using CalculatorLib;
+
+
+
+
+
+class Substraction : IOperation {
+    public override int Operate(int a, int b)
+    {
+        return a-b;
+    }
+};
+
+class Program{
+
+    static void Main(){
+
+        Calculator calc = new();
+        Console.WriteLine(calc.Calculate(1,1,(new Addition())));
+        Console.WriteLine(calc.Calculate(1,1,(new Substraction())));
+        return;
+    }
+}
