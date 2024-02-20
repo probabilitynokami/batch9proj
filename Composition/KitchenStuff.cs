900aeaf43827d9f0d1bbce05d5eb3f4042b18a3b
Dion Solang <gumball3point@gmail.com>
2024-02-20 10:17:30 +0700

    namespace conflict resolving

---
+++ Composition/KitchenStuff.cs
@@ -0,0 +1,10 @@
+namespace KitchenStuff;
+
+class Knife{
+    public float sharpness;
+}
+
+class Tire{
+    // yes.. i have tire in my kitchen, so what??
+    public float diameter;
+}
--- Composition/Program.cs
+++ Composition/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using KitchenStuff;
 using Vehicle;
 
 
@@ -8,7 +9,7 @@ class Program{
     static void Main(){
 
         Engine engine = new(8,2,"rotary");
-        Tire tire = new("michellin",100);
+        Vehicle.Tire tire = new("michellin",100);
         Car car = new(engine, tire, "hehe",1.0f);
 
         engine.type = "huhu";
