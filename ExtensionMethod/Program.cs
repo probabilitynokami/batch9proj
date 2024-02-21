275ac801edb0c9149c76aab2e8632b417bc344c7
Dion Solang <gumball3point@gmail.com>
2024-02-21 11:48:15 +0700

    extension method demo

---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+3.Hehe();
+3.Huhu(2);
+
+3.Add(2).Mul(3).Add(1).Hehe();
+
+static class ExtensionMethod{
+    public static void Hehe(this int x){
+        Console.WriteLine(x);
+    }
+    public static void Huhu(this int x, int y){
+        Console.WriteLine(x+y);
+    }
+
+    public static int Add(this int x, int y){
+        return x+y;
+    }
+    public static int Mul(this int x, int y){
+        return x*y;
+    }
+}
