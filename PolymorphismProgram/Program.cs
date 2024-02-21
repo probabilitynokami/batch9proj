6aa871f5a0021b2da4f5d3168cf6b6ac99c0390a
Dion Solang <gumball3point@gmail.com>
2024-02-21 09:05:17 +0700

    method overriding and cursed method hiding impl

--- PolymorphismProgram/Program.cs
+++ PolymorphismProgram/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+BaseClass a = new BaseClass();
+a.Call();
+a = new DerivedClass1();
+a.Call();
+a = new DerivedClass2();
+a.Call();
+Console.WriteLine("\n");
+(new DerivedClass1()).Call();
+(new DerivedClass2()).Call(); // method hiding
+
+
+class BaseClass{
+    public virtual void Call(){
+        Console.WriteLine("base class Called");
+    }
+}
+
+class DerivedClass1 : BaseClass {
+    public override void Call(){
+        Console.WriteLine("derived class1 Called");
+    }
+
+}
+
+class DerivedClass2 : BaseClass{
+    public void Call(){
+        Console.WriteLine("derived class2 Called");
+    }
+
+}
