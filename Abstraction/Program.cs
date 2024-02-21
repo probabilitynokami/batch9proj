8285d2956f4b9baaa6847961ff25d2bf91b208dc
Dion Solang <gumball3point@gmail.com>
2024-02-21 13:59:18 +0700

    abstraction demo

---
+++ Abstraction/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public abstract class A{
+    public abstract void ImplementThis();
+}
+
+public class B : A {
+    public override void ImplementThis()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+// Illegal
+// public class C : A {
+//  // did not implement ImplementThis()
+// }
+
+
+
+
