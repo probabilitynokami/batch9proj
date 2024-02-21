5b583e756b21f07b294177d4c21303796017ec23
Dion Solang <gumball3point@gmail.com>
2024-02-21 11:08:12 +0700

    statics demo finished

--- NPCInterractProgram/Program.cs
+++ NPCInterractProgram/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using NonPlayableCharacter;
+
+Monster monster1 = new(69);
+Monster monster2 = new(68);
+Monster monster3 = new(67);
+Monster monster4 = new(66);
+Blacksmith blacksmith1 = new(50);
+Swordsman swordsman1 = new(100);
+
+Console.WriteLine("total npcs: "+NPC.GetCount());
+
+var npcs = NPC.GetAll();
+
+foreach(NPC npc in npcs){
+    Console.WriteLine(npc.GetHealth());
+}
+
