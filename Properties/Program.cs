19900420df4e1780b256e5dc6a9d6ac8f6866977
Dion Solang <gumball3point@gmail.com>
2024-02-22 09:12:42 +0700

    properties demo

---
+++ Properties/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+PropertyDemo p = new();
+
+Console.WriteLine(p.readOnlyVar);
+p.setReadOnlyVar(9);
+Console.WriteLine(p.readOnlyVar);
+
+// p.readOnlyVar = 1; //compile error
+
+Console.WriteLine(p.readOnlyVar);
+
+// Console.WriteLine(p.WriteOnlyVar); //compile error
+
+class PropertyDemo {
+    public int readOnlyVar {get; private set;}
+    public int WriteOnlyVar {private get; set;}
+
+    public int readWriteOnlyVar {private get; set;}
+    public void setReadOnlyVar(int value){
+        this.readOnlyVar = value;
+    }
+
+}
