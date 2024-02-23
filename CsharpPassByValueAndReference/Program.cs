1f6f2d0545e923c2eed7c2aaa6ac6aa7311d0786
Dion Solang <gumball3point@gmail.com>
2024-02-23 08:27:46 +0700

    add c# argument passing (ref or val) demo

---
+++ CsharpPassByValueAndReference/Program.cs
@@ -0,0 +1,21 @@
+﻿class ReferenceTypeClass{
+    public int member;
+}
+
+
+
+class Program{
+    static void Main(){
+        int x = 0;
+        ReferenceTypeClass y = new(){member=0};
+        Function(x,y);
+        Console.WriteLine(x+" "+y.member); // output: 0 1
+    }
+
+    // lets make a parameter mutating function
+    // that takes a value type and reference type
+    static void Function(int x, ReferenceTypeClass y){
+        x = x+1;
+        y.member = y.member+1;
+    }
+}
