e52d79daf52d337976118736273ce0ea45e660af
Dion Solang <gumball3point@gmail.com>
2024-02-23 10:03:14 +0700

    added exception handling demo

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program{
+    static void Main(){
+        Console.WriteLine("select exception");
+        if(! int.TryParse(Console.ReadLine(), out int input)){
+            Console.WriteLine("input better value");
+            return;
+        }
+
+        try{
+            DoException(input);
+        }
+        catch(DivideByZeroException e){
+            Console.WriteLine("divby0: "+e);
+        }
+        catch(FormatException e){
+            Console.WriteLine("format: "+e);
+        }
+        catch(Exception e){
+            Console.WriteLine("otherexception: "+e);
+        }
+
+        Console.WriteLine("program end");
+        
+    }
+    static void DoException(int input){
+        if (input == 0){
+            int x = 1/input; // divisionby0exception
+            Console.WriteLine(x);
+            return;
+        }
+        if (input == 1){
+            throw new InternalBufferOverflowException();
+        }
+        if (input == 2){
+            throw new FormatException();
+        }
+        if (input == 3){
+            return;
+        }
+        throw new IndexOutOfRangeException();
+    }
+
+}
