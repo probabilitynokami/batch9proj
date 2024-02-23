a7cbef0d05c8733fcfe9fac0f264bb145e0cc5d1
Dion Solang <gumball3point@gmail.com>
2024-02-23 08:44:03 +0700

    added ref and out demo

--- CsharpPassByValueAndReference/Program.cs
+++ CsharpPassByValueAndReference/Program.cs
@@ -10,6 +10,14 @@ class Program{
         ReferenceTypeClass y = new(){member=0};
         Function(x,y);
         Console.WriteLine(x+" "+y.member); // output: 0 1
+
+        Function2(ref x);
+        Console.WriteLine(x); // output: 1
+
+        int a;
+        // Function2(ref a); // Illegal, ref require it to be assigned before being read
+        Function3(out a);
+        Console.WriteLine(a); // output: 4
     }
 
     // lets make a parameter mutating function
@@ -17,5 +25,16 @@ class Program{
     static void Function(int x, ReferenceTypeClass y){
         x = x+1;
         y.member = y.member+1;
+    }
+
+    // parameter mutating function with ValueType param
+    static void Function2(ref int x){ // ref kw -> like pointer but with some restriction. (see Main())
+        x = x+1;
+    }
+
+    static void Function3(out int x){
+        // Console.WriteLine(x); // illegal to read before assignment in method
+        x = 3;
+        x = x+1;
     }
 }
