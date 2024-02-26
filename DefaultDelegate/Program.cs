12c9b322fa0595b2514350178abcc36721a0d9f5
Dion Solang <gumball3point@gmail.com>
2024-02-26 09:00:00 +0700

    fix default delegate example

--- DefaultDelegate/Program.cs
+++ DefaultDelegate/Program.cs
@@ -10,12 +10,14 @@ class Program{
 	    // Func<T.., V> -> delegate V Action(T..)
 	    Func<int, float, int> functor =  ReturningFunction;
 
+		action.Invoke(1);
+		Console.WriteLine(functor.Invoke(1,2.0f));
 
     }
     static void NoReturnFunction(int x){
 	    Console.WriteLine(x);
     }
-    static int  NoReturnFunction(int x, float y){
+    static int  ReturningFunction(int x, float y){
 	    Console.WriteLine(x+y);
 	    return x;
     }
