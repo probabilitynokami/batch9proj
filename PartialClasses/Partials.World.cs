07b58faabbdfe20b1425842a6f028120cb873319
Dion Solang <gumball3point@gmail.com>
2024-02-26 11:55:29 +0700

    added partial class demo

---
+++ PartialClasses/Partials.World.cs
@@ -0,0 +1,7 @@
+namespace Partials;
+
+public partial class PartialClassDemo{
+    public void World(){
+        Console.WriteLine("world"+x);
+    }
+}
---
+++ PartialClasses/Program.cs
@@ -0,0 +1,20 @@
+﻿// Partial Class -> separate class to multifile
+
+using Partials;
+public partial class Program{
+
+    static void Main(){
+        PartialClassDemo p = new();
+        p.Hello();
+        p.World();
+    }
+}
+
+namespace Partials{
+    public partial class PartialClassDemo{
+        int x=69;
+        public void Hello(){
+            Console.WriteLine("Hello");
+        }
+    }
+}
