7280e2df9fbdd7f09588f22d48287d880d31cb7a
Dion Solang <gumball3point@gmail.com>
2024-02-26 08:52:22 +0700

    added default delegates example

---
+++ DefaultDelegate/Program.cs
@@ -0,0 +1,22 @@
+﻿
+class Program{
+    static void Main(){
+
+	    // Action<params...>
+	    // Action<T> -> delegate void Action(T t)
+	    Action<int> action = NoReturnFunction;
+
+	    // Func<params... , return type>
+	    // Func<T.., V> -> delegate V Action(T..)
+	    Func<int, float, int> functor =  ReturningFunction;
+
+
+    }
+    static void NoReturnFunction(int x){
+	    Console.WriteLine(x);
+    }
+    static int  NoReturnFunction(int x, float y){
+	    Console.WriteLine(x+y);
+	    return x;
+    }
+}
