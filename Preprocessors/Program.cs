8ca8f2f55124a75574a7e08b349c83e249aadd2e
Dion Solang <gumball3point@gmail.com>
2024-03-06 13:23:23 +0700

    add preprocessor demo

---
+++ Preprocessors/Program.cs
@@ -0,0 +1,14 @@
+﻿
+class Program{
+
+    static void Main(){
+#if BINBOWS
+        Console.WriteLine("BINBOWS");
+#elif LINOWS
+        Console.WriteLine("LINOWS");
+#endif
+
+        return;
+    }
+
+}
