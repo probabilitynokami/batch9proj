4b669e494de68b9db8d8348047649e24a44bc830
Dion Solang <gumball3point@gmail.com>
2024-03-06 14:22:27 +0700

    trying out dotnet watch hot reloading

---
+++ Debugger/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program{
+    static async Task Main(){
+        Func<int,int,int> myfunc = null;
+        ReftypeStuff rft = new();
+
+        HeheBoy(ref myfunc, rft);
+
+        while(true){
+            myfunc.Invoke(1,2);
+            Console.WriteLine(rft.a + " "+rft.b);
+            Thread.Sleep(100);
+            await Task.Delay(100);
+        }
+    }
+
+    static void HeheBoy(ref Func<int,int,int> myFunc, ReftypeStuff something){
+        var sus = (int l, int r) =>{
+            l = something.a;
+            l = l*2+1;
+            r = r*2+1;
+            int result = ((l-r)*4);
+            return result;
+        };
+        myFunc += sus;
+    }
+
+}
+
+class ReftypeStuff{
+    public int a = 0;
+    public int b = 0;
+}
