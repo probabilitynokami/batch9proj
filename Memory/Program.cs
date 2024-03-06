eff797cb5cf92bc4d6cf39f060d9ff27291d6325
Dion Solang <gumball3point@gmail.com>
2024-03-06 08:31:57 +0700

    commit things i forgot to commit

--- Memory/Program.cs
+++ Memory/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+
+class Program{
+
+    static void Main(){
+        Console.WriteLine("Hello, World!");
+
+        {
+            StringBuilder sb = new();
+
+            Console.ReadLine();
+
+            sb.Capacity = 64;
+            Console.ReadLine();
+
+            sb.Capacity = 100;
+            Console.ReadLine();
+
+            sb.Capacity = 100_000;
+            Console.ReadLine();
+
+            sb.Capacity = 1000_000;
+            Console.ReadLine();
+
+            sb.Capacity = 10_000_000;
+            Console.WriteLine("call gc now");
+            Console.ReadLine();
+
+            sb.Capacity = 10_000;
+            sb.Append(Enumerable.Repeat("a",9_999));
+            Console.ReadLine();
+        }
+
+        Console.WriteLine("stringbuilder should be freed rn, if not call GC");
+        Console.ReadLine();
+    }
+
+}
+
