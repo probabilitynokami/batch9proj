4407ed625c2f639b888bc9f8573b5852612a87b4
Dion Solang <gumball3point@gmail.com>
2024-03-25 15:00:28 +0700

    huge commit yey

---
+++ MVCTutorials/Controllers/CategoryController.cs
@@ -0,0 +1,85 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using MVCTutorial.Data;
+using MVCTutorial.Models;
+
+namespace MVCTutorial.Controllers;
+
+public class CategoryController : Controller
+{
+	private readonly DataContext _context;
+
+	public CategoryController(DataContext context)
+	{
+		_context = context;
+	}
+
+	public IActionResult Index()
+	{
+		var categories = _context.Categories;
+		return View(categories);
+	}
+
+	public IActionResult Update(int? id){
+		if(id is null){
+			TempData["error"] = $"id is null";
+			return RedirectToAction("Index");
+		}
+		var c = _context.Categories.Find(id);
+		if(c is null){
+			TempData["error"] = $"No Category of ID {id} found";
+			return RedirectToAction("Index");
+		}
+		return View(c);
+	}
+
+	[HttpPost]
+	public IActionResult Update(Category category){
+        Category old_category = _context.Categories.Find(category.CategoryId)!;
+		_context.Entry(old_category).State = EntityState.Detached;
+		if(old_category.CategoryName == category.CategoryName &&
+		   old_category.Description == category.Description){
+			TempData["info"] = $"No changes were made to Category of ID {category.CategoryId}";
+			return RedirectToAction("Index");
+		}
+		//_context.Categories.Update(category);
+		string query = "UPDATE Categories SET CategoryName='"+category.CategoryName.ToString()+ "' WHERE CategoryId="+category.CategoryId.ToString();
+		_context.Database.ExecuteSql($"{query}");
+		_context.SaveChanges();
+		TempData["info"] = $"Category of ID {category.CategoryId} successfully updated";
+		return RedirectToAction("Index");
+	}
+
+	public IActionResult Create() 
+	{
+		return View();
+	}
+
+	[HttpPost]
+	public IActionResult Create(Category category) 
+	{
+		//input the ccategory to database
+		_context.Categories.Add(category);
+		_context.SaveChanges();
+		return RedirectToAction("Index");
+	}
+
+	public IActionResult Delete(int? id){
+		if(id is null){
+			TempData["error"] = $"id is null";
+			return RedirectToAction("Index");
+		}
+
+		var c = _context.Categories.Find(id);
+
+		if(c is null){
+			TempData["error"] = $"No Category of ID {id} found";
+			return RedirectToAction("Index");
+		}
+
+		TempData["info"] = $"Category of ID {id} successfuly deleted";
+
+		return RedirectToAction("Index");
+	}
+}
--- mvc/Controllers/HomerController.cs
+++ mvc/Controllers/HomerController.cs
@@ -8,19 +8,40 @@ public class HomerController : Controller
 {
     private readonly ILogger<HomerController> _logger;
 
+    private string _now = "hello";
+    private int counter = 0;
+
     public HomerController(ILogger<HomerController> logger)
     {
         _logger = logger;
+        while(counter == 1){
+            ;
+        }
+        // Console.WriteLine("Homer created");
+        
     }
 
-    public IActionResult Indeo()
+    public IActionResult Index()
     {
         return View();
     }
 
     public IActionResult Privaco()
     {
-        return View();
+        counter++;
+        // if(_now == "hello"){
+        //     _now = "World";
+        // }
+        // else{
+        //     _now = "hello";
+        // }
+        _now = counter.ToString();
+        ViewResult view1;
+        ViewResult view2;
+        view1 = View(model:"<h2>HeLlo</h2>");
+        view2 = View(model:"hello");
+        // view1 = View("Index");
+        return view1;
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
